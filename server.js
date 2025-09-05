1d3fbf5434bd9dc217355e05d62dcd0cdce76307
spsocial <spsocial@users.noreply.github.com>
2025-09-06 00:08:24 +0700

    Fix Railway deployment configuration

--- server.js
+++ server.js
@@ -4,20 +4,31 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
-app.use(express.static(path.join(__dirname)));
+// Middleware to set proper content types
+app.use((req, res, next) => {
+  if (req.path.endsWith('.js')) {
+    res.type('application/javascript');
+  } else if (req.path.endsWith('.css')) {
+    res.type('text/css');
+  } else if (req.path.endsWith('.html')) {
+    res.type('text/html');
+  }
+  next();
+});
 
-// Serve index.html for root route
-app.get('/', (req, res) => {
+// Serve static files from current directory
+app.use(express.static(__dirname, {
+  extensions: ['html', 'css', 'js'],
+  index: 'index.html'
+}));
+
+// Catch all routes and serve index.html
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Handle 404
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(PORT, () => {
+// Start server
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Open http://localhost:${PORT} to view the app`);
 });
